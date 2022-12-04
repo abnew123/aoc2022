42dc14d1059ea112ca6d2a9c61b1192f53e2dc3f
abnew123 <shadowghosts31415@gmail.com>
2022-12-04 00:11:57 -0500

    Day4

--- src/aoc2022/Day4.java
+++ src/aoc2022/Day4.java
@@ -1,13 +1,29 @@
 package aoc2022;
 
-import java.io.*;
-import java.util.*;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Day4 extends DayTemplate{
 	
 	public String solve(boolean part1, Scanner in) throws FileNotFoundException {
 		int answer = 0;
-		
+		while(in.hasNext()) {
+			String[] elfranges = in.nextLine().split(",");
+			int elf1start = Integer.parseInt(elfranges[0].split("-")[0]);
+			int elf1end = Integer.parseInt(elfranges[0].split("-")[1]);
+			int elf2start = Integer.parseInt(elfranges[1].split("-")[0]);
+			int elf2end = Integer.parseInt(elfranges[1].split("-")[1]);
+			if(part1) {
+				if((elf1start >= elf2start && elf2end >= elf1end) || (elf1start <= elf2start && elf2end <= elf1end)) {
+					answer++;
+				}
+			}
+			else {
+				if((elf1start <= elf2end) && (elf2start <= elf1end)) {
+					answer++;
+				}
+			}
+		}
 		return "" + answer;
 	}
 }
