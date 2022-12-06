b66da6dc4e0fe68096f27dea4e6881bd43e3e1fb
abnew123 <shadowghosts31415@gmail.com>
2022-12-06 00:06:00 -0500

    Day6

--- src/aoc2022/Day6.java
+++ src/aoc2022/Day6.java
@@ -3,11 +3,22 @@ package aoc2022;
 import java.io.*;
 import java.util.*;
 
-public class Day6 extends DayTemplate{
-	
+public class Day6 extends DayTemplate {
+
 	public String solve(boolean part1, Scanner in) throws FileNotFoundException {
 		int answer = 0;
-		
+		String input = in.nextLine();
+		int codesize = part1 ? 4 : 14;
+		for (int i = 0; i < input.length() - codesize + 1; i++) {
+			Set<Character> code = new HashSet<>();
+			for (int j = 0; j < codesize; j++) {
+				code.add(input.charAt(i + j));
+			}
+			if (code.size() == codesize) {
+				answer = i + codesize;
+				break;
+			}
+		}
 		return "" + answer;
 	}
 }
