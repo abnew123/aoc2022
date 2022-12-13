2a346ce7dadb66e7e02b1771c22597207c0902ac
abnew123 <shadowghosts31415@gmail.com>
2022-12-12 21:45:36 -0500

    removed print line from solution

--- src/aoc2022/Day10.java
+++ src/aoc2022/Day10.java
@@ -32,12 +32,13 @@ public class Day10 extends DayTemplate {
 				screen[cycle / 40][cycle % 40] = (Math.abs(curVal - (cycle % 40)) < 2) ? '█' : ' ';
 				cycle++;
 			}
-			for (int i = 0; i < screen.length; i++) {
-				for (int j = 0; j < screen[0].length; j++) {
-					System.out.print(screen[i][j]);
-				}
-				System.out.println();
-			}
+			//uncomment below code to see the screen (which gives the answer returned). 
+//			for (int i = 0; i < screen.length; i++) {
+//				for (int j = 0; j < screen[0].length; j++) {
+//					System.out.print(screen[i][j]);
+//				}
+//				System.out.println();
+//			}
 			return "ECZUZALR";
 		}
 	}
