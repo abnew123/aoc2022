9d6ef80044524c70eb9ea30965ba227ac88d0a1c
abnew123 <shadowghosts31415@gmail.com>
2022-12-21 23:22:43 -0500

    cleaner implmentation of Day 21

--- src/aoc2022/Day21.java
+++ src/aoc2022/Day21.java
@@ -9,88 +9,124 @@ public class Day21 extends DayTemplate {
 		List<Monkey2> all = new ArrayList<>();
 		List<Monkey2> yelled = new ArrayList<>();
 		while (in.hasNext()) {
-			all.add(new Monkey2(in.nextLine(), part1));
+			String orig = in.nextLine();
+			if (orig.contains("root") && !part1) {
+				orig = orig.replace('+', '=');
+				orig = orig.replace('-', '=');
+				orig = orig.replace('/', '=');
+				orig = orig.replace('*', '=');
+			}
+			all.add(new Monkey2(orig, part1));
 		}
-		long[] res = helper(all, yelled, part1);
-		if (res != null) {
-			return "" + res[2];
-		}
-		int counter = 0;
-		long low = 0;
-		long high = 13238229598990L;
-		while (counter < 100) {
-			long med = (low + high) / 2;
-			List<Monkey2> newAll = new ArrayList<>();
-			for (int i = 0; i < all.size(); i++) {
-				newAll.add(new Monkey2(all.get(i).orig, part1));
-			}
-			for (int i = 0; i < newAll.size(); i++) {
-				if (newAll.get(i).name.equals("human")) {
-					newAll.get(i).name = "humn";
-					newAll.get(i).val = med;
-				}
-			}
-			List<Monkey2> newYelled = new ArrayList<>();
-			for (int i = 0; i < yelled.size(); i++) {
-				newYelled.add(new Monkey2(yelled.get(i).orig, part1));
-			}
-			newYelled.addAll(yelled);
-			long[] res2 = helper(newAll, newYelled, part1);
-			if (res2[0] > res2[1]) {
-				low = med;
-			}
-			if (res2[0] < res2[1]) {
-				high = med;
-			}
-			if (res2[0] == res2[1]) {
-				return "" + med;
-			}
-			counter++;
-		}
-		return "";
+		return helper(all, yelled, part1) + "";
 	}
 
-	public long[] helper(List<Monkey2> all, List<Monkey2> yelled, boolean part1) {
-		int size = all.size();
-		int prevSize = -1;
-		while (size != prevSize) {
-			prevSize = all.size();
+	public long helper(List<Monkey2> all, List<Monkey2> yelled, boolean part1) {
+		int solved = 0;
+		boolean[] solves = new boolean[all.size()];
+		while (solved < all.size()) {
 			for (int i = all.size() - 1; i >= 0; i--) {
-				long left = Long.MIN_VALUE;
-				long right = Long.MIN_VALUE;
-				for (int j = 0; j < yelled.size(); j++) {
-					if (yelled.get(j).name.equals(all.get(i).wait1)) {
-						left = yelled.get(j).val;
-					}
-					if (yelled.get(j).name.equals(all.get(i).wait2)) {
-						right = yelled.get(j).val;
+				if (solves[i]) {
+					continue;
+				}
+				if (all.get(i).orig.length() <= 10) {
+					if (!all.get(i).name.equals("humn") || part1) {
+						solved++;
+						solves[i] = true;
+						continue;
 					}
 				}
-				if (left != Long.MIN_VALUE && right != Long.MIN_VALUE) {
-					if (all.get(i).operation == 1) {
-						all.get(i).val = left + right;
-					}
-					if (all.get(i).operation == 2) {
-						all.get(i).val = left - right;
-					}
-					if (all.get(i).operation == 3) {
-						all.get(i).val = left * right;
-					}
-					if (all.get(i).operation == 4) {
-						all.get(i).val = left / right;
-					}
-					if (all.get(i).name.equals("root")) {
-						return new long[] { left, right, all.get(i).val };
-					}
+				if (!part1 && all.get(i).name.equals("humn") && all.get(i).val != Long.MIN_VALUE) {
+					return all.get(i).val;
 				}
-				if (all.get(i).val != Long.MIN_VALUE && (part1 || !all.get(i).name.equals("human"))) {
-					yelled.add(all.get(i));
-					all.remove(i);
+				if (!all.get(i).name.equals("humn")) {
+					Monkey2 left = null;
+					Monkey2 right = null;
+					for (int j = 0; j < all.size(); j++) {
+						if (all.get(j).name.equals(all.get(i).wait1)) {
+							left = all.get(j);
+						}
+						if (all.get(j).name.equals(all.get(i).wait2)) {
+							right = all.get(j);
+						}
+					}
+					boolean valUnknown = (all.get(i).val == Long.MIN_VALUE);
+					boolean leftUnknown = (left.val == Long.MIN_VALUE);
+					boolean rightUnknown = (right.val == Long.MIN_VALUE);
+					int known = valUnknown ? 0 : 1;
+					known += (left.val == Long.MIN_VALUE) ? 0 : 1;
+					known += (right.val == Long.MIN_VALUE) ? 0 : 1;
+					if (known == 3) {
+						solved++;
+						solves[i] = true;
+					}
+					if (known == 2 || (!part1 && known == 1 && all.get(i).name.equals("root"))) {
+						if (all.get(i).operation == 1) {
+							if (leftUnknown) {
+								left.val = all.get(i).val - right.val;
+							}
+							if (rightUnknown) {
+								right.val = all.get(i).val - left.val;
+							}
+							if (valUnknown) {
+								all.get(i).val = left.val + right.val;
+							}
+
+						}
+						if (all.get(i).operation == 2) {
+							if (leftUnknown) {
+								left.val = all.get(i).val + right.val;
+							}
+							if (rightUnknown) {
+								right.val = left.val - all.get(i).val;
+							}
+							if (valUnknown) {
+								all.get(i).val = left.val - right.val;
+							}
+						}
+						if (all.get(i).operation == 3) {
+							if (leftUnknown) {
+								left.val = all.get(i).val / right.val;
+							}
+							if (rightUnknown) {
+								right.val = all.get(i).val / left.val;
+							}
+							if (valUnknown) {
+								all.get(i).val = left.val * right.val;
+							}
+						}
+						if (all.get(i).operation == 4) {
+							if (leftUnknown) {
+								left.val = all.get(i).val * right.val;
+							}
+							if (rightUnknown) {
+								right.val = left.val / all.get(i).val;
+							}
+							if (valUnknown) {
+								all.get(i).val = left.val / right.val;
+							}
+						}
+						if (all.get(i).operation == 5) {
+							if (leftUnknown) {
+								left.val = right.val;
+							}
+							if (rightUnknown) {
+								right.val = left.val;
+							}
+							solved++;
+							solves[i] = true;
+						}
+					}
 				}
 			}
-			size = all.size();
 		}
-		return null;
+		long answer = 0;
+		for (int i = 0; i < all.size(); i++) {
+			if (all.get(i).name.equals("root")) {
+				answer = all.get(i).val;
+			}
+		}
+		return answer;
 	}
 }
 
@@ -105,9 +141,6 @@ class Monkey2 {
 	public Monkey2(String line, boolean part1) {
 		orig = line;
 		name = line.split(":")[0];
-		if (name.equals("humn") && !part1) {
-			name = "human";
-		}
 		if (line.length() > 10) {
 			val = Long.MIN_VALUE;
 			wait1 = line.split(" ")[1];
@@ -125,11 +158,14 @@ class Monkey2 {
 			if (op.equals("/")) {
 				operation = 4;
 			}
-			if (name.equals("root") && !part1) {
+			if (op.equals("=")) {
 				operation = 5;
 			}
 		} else {
 			val = Long.parseLong(line.split(" ")[1]);
+		}
+		if (name.equals("humn") && !part1) {
+			val = Long.MIN_VALUE;
 		}
 	}
 }
