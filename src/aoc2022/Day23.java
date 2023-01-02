abb9bb4aee49aa1f3087a9fe2d461f770641664f
abnew123 <shadowghosts31415@gmail.com>
2023-01-01 23:10:24 -0500

    part 2 speedup from 150 seconds to 0.3

--- src/aoc2022/Day23.java
+++ src/aoc2022/Day23.java
@@ -37,7 +37,15 @@ public class Day23 extends DayTemplate {
 				}
 			}
 		} else {
-			answer = helper2(elves, -1);
+			answer = helper2(elves, 2000);
+			int maxx, maxy, minx, miny;
+			maxx = maxy = minx = miny = -1;
+			for (int i = 0; i < elves.size(); i++) {
+				maxx = (maxx == -1) ? elves.get(i).x : Math.max(maxx, elves.get(i).x);
+				maxy = (maxy == -1) ? elves.get(i).y : Math.max(maxy, elves.get(i).y);
+				minx = (minx == -1) ? elves.get(i).x : Math.min(minx, elves.get(i).x);
+				miny = (miny == -1) ? elves.get(i).y : Math.min(miny, elves.get(i).y);
+			}
 		}
 		return "" + answer;
 	}
@@ -68,74 +76,21 @@ public class Day23 extends DayTemplate {
 	}
 
 	public int helper2(List<Coord2> elves, int rounds) {
-		if (rounds > 0) {
-			for (int i = 0; i < rounds; i++) {
-				Coord2[] proposed = new Coord2[elves.size()];
-				for (int j = 0; j < elves.size(); j++) {
-					int attempt = 0;
-					List<Coord2> neighbors = new ArrayList<>();
-					for (int k = 0; k < 4; k++) {
-						neighbors.addAll(helper(elves.get(j), i, attempt + k));
-					}
-					boolean empty = true;
-					for (Coord2 c : neighbors) {
-						if (elves.contains(c)) {
-							empty = false;
-						}
-					}
-					if (empty) {
-						proposed[j] = null;
-						continue;
-					}
-					boolean flag = true;
-					while (flag) {
-						List<Coord2> possible = helper(elves.get(j), i, attempt);
-						boolean empty2 = true;
-						for (Coord2 c : possible) {
-							if (elves.contains(c)) {
-								empty2 = false;
-							}
-						}
-						if (empty2) {
-							proposed[j] = possible.get(1);
-							flag = false;
-						}
-						attempt++;
-						if (attempt == 5) {
-							break;
-						}
-					}
-				}
-				boolean[] dupe = new boolean[proposed.length];
-				for (int j = 0; j < proposed.length; j++) {
-					for (int k = j + 1; k < proposed.length; k++) {
-						if (proposed[j] != null && proposed[j].equals(proposed[k])) {
-							dupe[j] = true;
-							dupe[k] = true;
-						}
-					}
-				}
-				for (int j = 0; j < elves.size(); j++) {
-					if (!dupe[j] && proposed[j] != null) {
-						elves.set(j, proposed[j]);
-					}
-				}
-			}
-			return -1;
+		int[][] grid = new int[300][300];
+		for (Coord2 elf : elves) {
+			grid[elf.x + 100][elf.y + 100] = 1;
 		}
-		int counter = 0;
-		boolean allNull = false;
-		while(!allNull) {
-			allNull = true;
+		int[] xneighbors = new int[] { -1, 0, 1, -1, 1, -1, 0, 1 };
+		int[] yneighbors = new int[] { -1, -1, -1, 0, 0, 1, 1, 1 };
+		for (int i = 0; i < rounds; i++) {
+			Map<Coord2, Integer> freq = new HashMap<>();
+			boolean allNull = true;
 			Coord2[] proposed = new Coord2[elves.size()];
 			for (int j = 0; j < elves.size(); j++) {
-				List<Coord2> neighbors = new ArrayList<>();
-				for (int k = 0; k < 4; k++) {
-					neighbors.addAll(helper(elves.get(j), counter, k));
-				}
+				Coord2 elf = elves.get(j);
 				boolean empty = true;
-				for (Coord2 c : neighbors) {
-					if (elves.contains(c)) {
+				for (int k = 0; k < xneighbors.length; k++) {
+					if (grid[elf.x + xneighbors[k] + 100][elf.y + yneighbors[k] + 100] == 1) {
 						empty = false;
 					}
 				}
@@ -143,38 +98,41 @@ public class Day23 extends DayTemplate {
 					proposed[j] = null;
 					continue;
 				}
-				for(int k = 0; k < 4; k++) {
-					List<Coord2> possible = helper(elves.get(j), counter, k);
+				for (int k = 0; k < 4; k++) {
+					List<Coord2> possible = helper(elves.get(j), i, k);
 					boolean empty2 = true;
 					for (Coord2 c : possible) {
-						if (elves.contains(c)) {
+						if (grid[c.x + 100][c.y + 100] == 1) {
 							empty2 = false;
 						}
 					}
 					if (empty2) {
 						proposed[j] = possible.get(1);
+						if (freq.keySet().contains(proposed[j])) {
+							freq.put(proposed[j], freq.get(proposed[j]) + 1);
+						} else {
+							freq.put(proposed[j], 1);
+						}
 						break;
 					}
 				}
 			}
-			boolean[] dupe = new boolean[proposed.length];
 			for (int j = 0; j < proposed.length; j++) {
-				for (int k = j + 1; k < proposed.length; k++) {
-					if (proposed[j] != null && proposed[j].equals(proposed[k])) {
-						dupe[j] = true;
-						dupe[k] = true;
-					}
-				}
-			}
-			for (int j = 0; j < elves.size(); j++) {
-				if (!dupe[j] && proposed[j] != null) {
+				if (proposed[j] != null && freq.get(proposed[j]) == 1) {
 					elves.set(j, proposed[j]);
 					allNull = false;
 				}
 			}
-			counter++;
+
+			if (allNull) {
+				return i + 1;
+			}
+			grid = new int[300][300];
+			for (Coord2 elf : elves) {
+				grid[elf.x + 100][elf.y + 100] = 1;
+			}
 		}
-		return counter;
+		return -1;
 	}
 }
 
@@ -197,5 +155,10 @@ class Coord2 {
 			return x == o.x && y == o.y;
 		}
 		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return 1000 * x + y;
 	}
 }
