84bc9a943f2affba8f905a9d37c72c2e75c4707f
abnew123 <shadowghosts31415@gmail.com>
2023-01-07 01:53:03 -0500

    only recompute blizz arr if time goes up, around 6x speedup

--- src/aoc2022/Day24.java
+++ src/aoc2022/Day24.java
@@ -10,13 +10,14 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Day24 extends DayTemplate {
-
 	public String solve(boolean part1, Scanner in) throws FileNotFoundException {
 		List<String> lines = new ArrayList<>();
 		Map<Integer, List<CoordTime>> blizzards = new HashMap<>();
 		while (in.hasNext()) {
 			lines.add(in.nextLine());
 		}
+		int width = lines.size();
+		int height = lines.get(0).length();
 		List<CoordTime> initBlizzards = new ArrayList<>();
 		CoordTime start = null;
 		boolean first = true;
@@ -49,47 +50,44 @@ public class Day24 extends DayTemplate {
 		}
 		blizzards.put(0, initBlizzards);
 		for (int i = 1; i < 1000; i++) {
-			blizzards.put(i, advance(blizzards.get(i - 1), lines));
+			blizzards.put(i, advance(blizzards.get(i - 1), width, height));
 		}
-		int trip1 = helper(start, end, lines, blizzards, 0);
+		int trip1 = helper(start, end, width, height, blizzards, 0);
 		if (part1) {
 			return "" + trip1;
 		}
-		int trip2 = helper(end, start, lines, blizzards, trip1);
-		int trip3 = helper(start, end, lines, blizzards, trip2);
+		int trip2 = helper(end, start, width, height, blizzards, trip1);
+		int trip3 = helper(start, end, width, height, blizzards, trip2);
 		return "" + trip3;
 	}
 
-	public boolean inBounds(CoordTime candidate, List<String> lines) {
+	public boolean inBounds(CoordTime candidate, int width, int height) {
 		if (candidate.direction == -1) {
-			return (candidate.x >= 0 && candidate.y >= 0 && candidate.x < lines.size()
-					&& candidate.y < lines.get(0).length());
+			return (candidate.x >= 0 && candidate.y >= 0 && candidate.x < width && candidate.y < height);
 		}
-		return (candidate.x >= 1 && candidate.y >= 1 && candidate.x < lines.size() - 1
-				&& candidate.y < lines.get(0).length() - 1);
+		return (candidate.x >= 1 && candidate.y >= 1 && candidate.x < width - 1 && candidate.y < height - 1);
 	}
 
-	public List<CoordTime> advance(List<CoordTime> current, List<String> lines) {
+	public List<CoordTime> advance(List<CoordTime> current, int width, int height) {
 		List<CoordTime> next = new ArrayList<>();
 		for (CoordTime blizzard : current) {
 			int deltax = (blizzard.direction == 1) ? -1 : (blizzard.direction == 3) ? 1 : 0;
 			int deltay = (blizzard.direction == 4) ? -1 : (blizzard.direction == 2) ? 1 : 0;
 			CoordTime candidate = new CoordTime(blizzard.x + deltax, blizzard.y + deltay, blizzard.time + 1,
 					blizzard.direction);
-			if (inBounds(candidate, lines)) {
+			if (inBounds(candidate, width, height)) {
 				next.add(candidate);
 			} else {
 				if (candidate.x == 0) {
-					next.add(new CoordTime(lines.size() - 2, candidate.y, candidate.time, candidate.direction));
+					next.add(new CoordTime(width - 2, candidate.y, candidate.time, candidate.direction));
 				}
-				if (candidate.x == lines.size() - 1) {
+				if (candidate.x == width - 1) {
 					next.add(new CoordTime(1, candidate.y, candidate.time, candidate.direction));
 				}
 				if (candidate.y == 0) {
-					next.add(
-							new CoordTime(candidate.x, lines.get(0).length() - 2, candidate.time, candidate.direction));
+					next.add(new CoordTime(candidate.x, height - 2, candidate.time, candidate.direction));
 				}
-				if (candidate.y == lines.get(0).length() - 1) {
+				if (candidate.y == height - 1) {
 					next.add(new CoordTime(candidate.x, 1, candidate.time, candidate.direction));
 				}
 			}
@@ -97,23 +95,36 @@ public class Day24 extends DayTemplate {
 		return next;
 	}
 
-	public int helper(CoordTime start, CoordTime end, List<String> lines, Map<Integer, List<CoordTime>> blizzards,
+	public int helper(CoordTime start, CoordTime end, int width, int height, Map<Integer, List<CoordTime>> blizzards,
 			int startTime) {
 		Queue<CoordTime> bfs = new LinkedList<>();
+		Queue<CoordTime> bfs2 = new LinkedList<>();
+		int[][] blizz = new int[width][height];
+		int[] deltax = new int[] { -1, 0, 1, 0, 0 };
+		int[] deltay = new int[] { 0, -1, 0, 1, 0 };
+		int timestamp = startTime;
 		bfs.add(new CoordTime(start.x, start.y, startTime, -1));
-		while (!bfs.isEmpty()) {
+		while (!bfs.isEmpty() || !bfs2.isEmpty()) {
+			if(bfs.isEmpty()) {
+				timestamp++;
+				bfs.addAll(bfs2);
+				bfs2 = new LinkedList<>();
+				blizz = new int[width][height];
+				List<CoordTime> nextBlizzards = blizzards.get(timestamp + 1);
+				for (CoordTime c : nextBlizzards) {
+					blizz[c.x][c.y] += 1;
+				}
+			}
 			CoordTime next = bfs.poll();
 			if (next.x == end.x && next.y == end.y) {
 				return next.time;
 			}
-			List<CoordTime> nextBlizzards = blizzards.get(next.time + 1);
-			int[] deltax = new int[] { -1, 0, 1, 0, 0 };
-			int[] deltay = new int[] { 0, -1, 0, 1, 0 };
 			for (int i = 0; i < deltax.length; i++) {
 				CoordTime candidate = new CoordTime(next.x + deltax[i], next.y + deltay[i], next.time + 1,
 						next.direction);
-				if (!nextBlizzards.contains(candidate) && inBounds(candidate, lines) && !bfs.contains(candidate)) {
-					bfs.add(candidate);
+				if (inBounds(candidate, width, height) && blizz[candidate.x][candidate.y] == 0 
+						&& !bfs2.contains(candidate)) {
+					bfs2.add(candidate);
 				}
 			}
 		}
