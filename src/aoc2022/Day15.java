2756b8efc22d5ff08426f1d90307ee524cce106d
abnew123 <shadowghosts31415@gmail.com>
2023-01-16 19:16:30 -0500

    3x faster part 1. further optimizations probably involve range set

--- src/aoc2022/Day15.java
+++ src/aoc2022/Day15.java
@@ -7,6 +7,10 @@ import java.util.Scanner;
 
 public class Day15 extends DayTemplate {
 
+	public int[] sensorx;
+	public int[] sensory;
+	public int[] distArray;
+
 	public String solve(boolean part1, Scanner in) throws FileNotFoundException {
 		long answer = 0;
 		List<Coord> sensors = new ArrayList<>();
@@ -26,14 +30,24 @@ public class Day15 extends DayTemplate {
 			beacons.add(new Coord(x2, y2));
 			distances.add(Math.abs(x1 - x2) + Math.abs(y1 - y2));
 		}
+		sensorx = new int[sensors.size()];
+		sensory = new int[sensors.size()];
+		distArray = new int[sensors.size()];
+		for (int i = 0; i < sensors.size(); i++) {
+			sensorx[i] = sensors.get(i).x;
+			sensory[i] = sensors.get(i).y;
+			distArray[i] = distances.get(i);
+		}
 		if (part1) {
-			for (int x = -5000000; x < 5000000; x++) {
+			for (int x = -1000000; x < 5000000; x++) {
 				if (alreadyBeacons.contains(x)) {
 					continue;
 				}
-				boolean possible = checkPossible(x, 2000000, sensors, distances, 0);
-				if (!possible) {
+				int possible = checkPossible(x, 2000000);
+				if (possible < 0) {
 					answer++;
+				} else {
+					x += possible;
 				}
 			}
 		}
@@ -52,7 +66,7 @@ public class Day15 extends DayTemplate {
 						int x = (b - a) / 2;
 						int y = (b + a) / 2;
 						if (x >= 0 && y >= 0 && x <= 4000000 && y <= 4000000) {
-							if (checkPossible(x, y, sensors, distances, 0)) {
+							if (checkPossible(x, y) >= 0) {
 								return "" + (((long) x * 4000000) + y);
 							}
 						}
@@ -63,13 +77,16 @@ public class Day15 extends DayTemplate {
 		return "" + answer;
 	}
 
-	public boolean checkPossible(int x, int y, List<Coord> sensors, List<Integer> distances, int wiggle) {
-		for (int i = 0; i < sensors.size(); i++) {
-			if (Math.abs(x - sensors.get(i).x) + Math.abs(y - sensors.get(i).y) + wiggle <= distances.get(i)) {
-				return false;
+	public int checkPossible(int x, int y) {
+		int m = 1000000;
+		for (int i = 0; i < sensorx.length; i++) {
+			if (Math.abs(x - sensorx[i]) + Math.abs(y - sensory[i]) <= distArray[i]) {
+				return -1;
+			} else {
+				m = Math.min(m, Math.abs(x - sensorx[i]) + Math.abs(y - sensory[i]) - distArray[i]);
 			}
 		}
-		return true;
+		return m - 1;
 	}
 }
 
