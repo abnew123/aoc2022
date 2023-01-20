c2ea88ffcf3741d775ef773d630e2549931bd82a
abnew123 <shadowghosts31415@gmail.com>
2023-01-20 18:50:52 -0500

    100x speedup using ranges over coords

--- src/aoc2022/Day15.java
+++ src/aoc2022/Day15.java
@@ -2,14 +2,11 @@ package aoc2022;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Day15 extends DayTemplate {
-
-	public int[] sensorx;
-	public int[] sensory;
-	public int[] distArray;
 
 	public String solve(boolean part1, Scanner in) throws FileNotFoundException {
 		long answer = 0;
@@ -17,39 +14,52 @@ public class Day15 extends DayTemplate {
 		List<Coord> beacons = new ArrayList<>();
 		List<Integer> distances = new ArrayList<>();
 		List<Integer> alreadyBeacons = new ArrayList<>();
+		List<Range> ranges = new ArrayList<>();
 		while (in.hasNext()) {
 			String[] line = in.nextLine().split("=|,|:");
 			int x1 = Integer.parseInt(line[1]);
 			int y1 = Integer.parseInt(line[3]);
 			int x2 = Integer.parseInt(line[5]);
 			int y2 = Integer.parseInt(line[7]);
-			if (y2 == 2000000) {
+			if (y2 == 2000000 && !alreadyBeacons.contains(x2)) {
 				alreadyBeacons.add(x2);
 			}
 			sensors.add(new Coord(x1, y1));
 			beacons.add(new Coord(x2, y2));
 			distances.add(Math.abs(x1 - x2) + Math.abs(y1 - y2));
 		}
-		sensorx = new int[sensors.size()];
-		sensory = new int[sensors.size()];
-		distArray = new int[sensors.size()];
-		for (int i = 0; i < sensors.size(); i++) {
-			sensorx[i] = sensors.get(i).x;
-			sensory[i] = sensors.get(i).y;
-			distArray[i] = distances.get(i);
-		}
 		if (part1) {
-			for (int x = -1000000; x < 5000000; x++) {
-				if (alreadyBeacons.contains(x)) {
-					continue;
-				}
-				int possible = checkPossible(x, 2000000);
-				if (possible < 0) {
-					answer++;
-				} else {
-					x += possible;
+			for (int i = 0; i < sensors.size(); i++) {
+				int dy = Math.abs(sensors.get(i).y - 2000000);
+				if (dy <= distances.get(i)) {
+					int dx = distances.get(i) - dy;
+					ranges.add(new Range(sensors.get(i).x - dx, sensors.get(i).x + dx));
 				}
 			}
+			Collections.sort(ranges);
+			int s = ranges.get(0).start;
+			int e = ranges.get(0).end;
+			for (Range r : ranges) {
+				if (r.start > e) {
+					for(int b: alreadyBeacons) {
+						if(b >= s && b <= e) {
+							answer--;
+						}
+					}
+					answer += e - s + 1;
+					s = r.start;
+					e = r.end;
+				}
+				if (r.end > e) {
+					e = r.end;
+				}
+			}
+			for(int b: alreadyBeacons) {
+				if(b >= s && b <= e) {
+					answer--;
+				}
+			}
+			answer += e - s + 1;
 		}
 		if (!part1) {
 			List<Integer> positiveLines = new ArrayList<>();
@@ -66,7 +76,7 @@ public class Day15 extends DayTemplate {
 						int x = (b - a) / 2;
 						int y = (b + a) / 2;
 						if (x >= 0 && y >= 0 && x <= 4000000 && y <= 4000000) {
-							if (checkPossible(x, y) >= 0) {
+							if (checkPossible(x, y, sensors, distances) >= 0) {
 								return "" + (((long) x * 4000000) + y);
 							}
 						}
@@ -77,13 +87,13 @@ public class Day15 extends DayTemplate {
 		return "" + answer;
 	}
 
-	public int checkPossible(int x, int y) {
+	public int checkPossible(int x, int y, List<Coord> sensors, List<Integer> distances) {
 		int m = 1000000;
-		for (int i = 0; i < sensorx.length; i++) {
-			if (Math.abs(x - sensorx[i]) + Math.abs(y - sensory[i]) <= distArray[i]) {
+		for (int i = 0; i < sensors.size(); i++) {
+			if (Math.abs(x - sensors.get(i).x) + Math.abs(y - sensors.get(i).y) <= distances.get(i)) {
 				return -1;
 			} else {
-				m = Math.min(m, Math.abs(x - sensorx[i]) + Math.abs(y - sensory[i]) - distArray[i]);
+				m = Math.min(m, Math.abs(x - sensors.get(i).x) + Math.abs(y - sensors.get(i).y) - distances.get(i));
 			}
 		}
 		return m - 1;
@@ -110,5 +120,30 @@ class Coord {
 		}
 		Coord o = (Coord) obj;
 		return o.x == x && o.y == y;
+	}
+}
+
+class Range implements Comparable<Range> {
+	int start;
+	int end;
+
+	public Range(int s, int e) {
+		start = s;
+		end = e;
+	}
+
+	@Override
+	public int compareTo(Range o) {
+		Range other = (Range) o;
+		if (other.start != start) {
+			return start - other.start;
+		} else {
+			return end - other.end;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return start + " " + end;
 	}
 }
