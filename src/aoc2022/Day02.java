7a2ea3bab173c55dcb762925b53411101e9138a7
abnew123 <shadowghosts31415@gmail.com>
2026-07-05 02:08:21 -0400

    Optimize Day2 scoring parse

--- src/aoc2022/Day02.java
+++ src/aoc2022/Day02.java
@@ -1,29 +1,22 @@
 package aoc2022;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Day02 extends DayTemplate {
 
 	public String solve(boolean part1, Scanner in) throws FileNotFoundException {
 		int answer = 0;
-		List<String> lines = new ArrayList<>();
-		while (in.hasNext()) {
-			lines.add(in.nextLine());
-		}
-		Map<String, Integer> RPS = Map.of("A", 0, "B", 1, "C", 2, "X", 3, "Y", 4, "Z", 5);
-		for (String line : lines) {
-			int opp = RPS.get(line.split(" ")[0]);
-			int me = RPS.get(line.split(" ")[1]);
+		while (in.hasNextLine()) {
+			String line = in.nextLine();
+			int opp = line.charAt(0) - 'A';
+			int me = line.charAt(2) - 'X';
 			if (part1) {
-				answer += me % 3 + 1; // from throw
-				answer += (opp % 3 == me % 3) ? 3 : ((opp + 1) % 3 == me % 3) ? 6 : 0; // from outcome
+				answer += me + 1;
+				answer += opp == me ? 3 : (opp + 1) % 3 == me ? 6 : 0;
 			} else {
-				answer += (me - 3) * 3; // from outcome
-				answer += ((me == 3) ? (opp + 2) % 3 : (me == 5) ? (opp + 1) % 3 : opp) + 1;// from throw
+				answer += me * 3;
+				answer += (me == 0 ? (opp + 2) % 3 : me == 2 ? (opp + 1) % 3 : opp) + 1;
 			}
 		}
 		return "" + answer;
