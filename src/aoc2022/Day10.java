62caf8a6b91de07adbafaa681ae640d8d32d38b3
abnew123 <shadowghosts31415@gmail.com>
2026-07-05 03:41:48 -0400

    Decode 2022 Day10 CRT output

--- src/aoc2022/Day10.java
+++ src/aoc2022/Day10.java
@@ -4,6 +4,9 @@ import java.io.*;
 import java.util.*;
 
 public class Day10 extends DayTemplate {
+	static int[] G = {422148690, 959017564, 422068812, 1024344606, 1024344592, 422074958, 623856210,
+			203491916, 625758866, 554189342, 959017488, 959017618, 623462988, 1008869918};
+	static String L = "ABCEFGHJKLPRUZ";
 
 	public String solve(boolean part1, Scanner in) throws FileNotFoundException {
 		int answer = 0;
@@ -39,7 +42,21 @@ public class Day10 extends DayTemplate {
 
 
 
-			return "ECZUZALR";
+			String ans = "";
+			for (int ch = 0; ch < 8; ch++) {
+				int g = 0;
+				for (int r = 0; r < 6; r++) {
+					for (int c = 0; c < 5; c++) {
+						g = g * 2 + (screen[r][ch * 5 + c] == '█' ? 1 : 0);
+					}
+				}
+				for (int i = 0; i < G.length; i++) {
+					if (G[i] == g) {
+						ans += L.charAt(i);
+					}
+				}
+			}
+			return ans;
 		}
 	}
 }
