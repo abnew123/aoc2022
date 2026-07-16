8fc0d4f499f434ff88d9e4bf7d595d3226c97b54
abnew123 <shadowghosts31415@gmail.com>
2026-07-16 14:37:20 -0400

    Tighten Days 16, 17, and 19

--- src/aoc2022/Day17Golfed.java
+++ src/aoc2022/Day17Golfed.java
@@ -1,43 +1,40 @@
 package aoc2022;
 import java.util.*;
 class Q{
-int[]P={15,132866,263175,16843009,771},h;List<Integer>c;
+int[]P={15,132866,263175,16843009,771};byte[]c;int h;
 String s(boolean p,String[]j){
-var J=j[0];
-c=$.a();var m=$.h();
-h=new int[7];long n=p?2022:0xe8d4a51000L,r=0,e=0;int q=0;
+c=new byte[1<<16];h=0;var J=j[0];var m=$.h();long n=p?2022:0xe8d4a51000L,r=0,e=0;int q=0;
 for(;r<n;r++){
-int k=$.f(r,5),x=2,y=c.size()+3;
+int k=$.f(r,5),x=2,y=h+3;
 for(;;){
 int X=x+(J.charAt(q)<61?-1:1);q=++q%J.length();
 if(o(P[k],X,y))x=X;
 if(o(P[k],x,y-1))y--;else{u(P[k],x,y);break;}
 }
 if(e<1){
-var v=(long[])m.put(k(r+1,q),new long[]{r+1,c.size()});
-if(v!=null){long a=r+1-v[0],g=(n-r-1)/a;r+=g*a;e=g*(c.size()-v[1]);}
+var v=(long[])m.put(k(r+1,q),new long[]{r+1,h});
+if(v!=null){long a=r+1-v[0],g=(n-r-1)/a;r+=g*a;e=g*(h-v[1]);}
 }
 }
-return e+c.size()+"";
+return e+h+"";
 }
 boolean o(int p,int x,int y){
-if((x|y)<0)return 0>1;
+if((x|y)<0)return false;
 for(;p>0;p>>=8,y++){
 int w=p%256<<x;
-if(w>127||y<c.size()&&(w&c.get(y))>0)return 0>1;
+if(w>127||y<c.length&&(w&c[y])>0)return false;
 }
-return 1>0;
+return true;
 }
 void u(int p,int x,int y){
 for(;p>0;p>>=8,y++){
-for(;c.size()<=y;)c.add(0);
-int w=p%256<<x;c.set(y,c.get(y)|w);
-for(int i=7;i-->0;)if((w&1<<i)>0)h[i]=$.x(h[i],y+1);
+if(y==c.length)c=Arrays.copyOf(c,y*2);
+int w=p%256<<x;c[y]|=w;h=Math.max(h,y+1);
 }
 }
 String k(long r,int q){
-int f=c.size(),z=f;
-for(int x:h)f=f<x?f:x;
-return r%5+","+q+c.subList(f,z);
+int f=h;
+for(int x=7;x-->0;)for(int y=h;y-->0;)if((c[y]&1<<x)>0){f=Math.min(f,y+1);break;}
+return r%5+","+q+":"+new String(c,0,f,h-f);
 }
 }
--- src/aoc2022/Day19Golfed.java
+++ src/aoc2022/Day19Golfed.java
@@ -1,6 +1,6 @@
 package aoc2022;
 class S{
-int q[][],m[],z;
+int q[],m[],z;
 String s(boolean p,String[]I){
 int r=p?0:1,j=0;
 for(var l:I)if(j++<3|p){var x=l.split("\\D+");int v=f(x,p?24:32);r=p?r+$.i(x[1])*v:r*v;}
@@ -8,7 +8,7 @@ return""+r;
 }
 int f(String[]x,int t){
 int a=$.i(x[2]),e=$.i(x[3]),f=$.i(x[4]),h=$.i(x[5]),o=$.i(x[6]),c=$.i(x[7]);
-q=new int[][]{{a,0,0},{e,0,0},{f,h,0},{o,0,c}};m=new int[]{$.x($.x(a,e),$.x(f,o)),h,c,t};z=0;
+q=new int[]{a,0,0,e,0,0,f,h,0,o,0,c};m=new int[]{$.x($.x(a,e),$.x(f,o)),h,c,t};z=0;
 d(t,1,0,0,0,0,0,0,0);return z;
 }
 void d(int t,int a,int e,int f,int h,int o,int c,int b,int g){
@@ -16,8 +16,8 @@ z=$.x(z,g+h*t);if(g+h*t+t*(t-1)/2<=z)return;
 int[]r={a,e,f,h},v={o,c,b};
 for(int i=4;i-->0;)if(r[i]<m[i]){
 int w=0;
-for(int j=3;j-->0;)if(v[j]<q[i][j])w=$.x(w,r[j]<1?t:(q[i][j]-v[j]+r[j]-1)/r[j]);
-if(w++<t)d(t-w,a+(i==0?1:0),e+(i==1?1:0),f+(i==2?1:0),h+(i==3?1:0),o+a*w-q[i][0],c+e*w-q[i][1],b+f*w-q[i][2],g+h*w);
+for(int j=3;j-->0;)if(v[j]<q[i*3+j])w=$.x(w,r[j]<1?t:(q[i*3+j]-v[j]+r[j]-1)/r[j]);
+if(w++<t)d(t-w,a+(i<1?1:0),e+(i==1?1:0),f+(i==2?1:0),h+(i>2?1:0),o+a*w-q[i*3],c+e*w-q[i*3+1],b+f*w-q[i*3+2],g+h*w);
 }
 }
 }
