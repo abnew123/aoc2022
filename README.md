6e572477a49954755ff03ad5e1388249b06f2cc9
abnew123 <shadowghosts31415@gmail.com>
2026-07-17 13:02:34 -0400

    Golf Days 22 and 23

--- README.md
+++ README.md
@@ -97,10 +97,10 @@ The table below counts non-whitespace characters in each normal solution file an
 | 19 | [Source](src/aoc2022/Day19.java) | [Golfed](src/aoc2022/Day19Golfed.java) | 2,934 | 717 |
 | 20 | [Source](src/aoc2022/Day20.java) | [Golfed](src/aoc2022/Day20Golfed.java) | 1,029 | 353 |
 | 21 | [Source](src/aoc2022/Day21.java) | [Golfed](src/aoc2022/Day21Golfed.java) | 3,048 | 659 |
-| 22 | [Source](src/aoc2022/Day22.java) | [Golfed](src/aoc2022/Day22Golfed.java) | 2,720 | 870 |
-| 23 | [Source](src/aoc2022/Day23.java) | [Golfed](src/aoc2022/Day23Golfed.java) | 3,302 | 687 |
+| 22 | [Source](src/aoc2022/Day22.java) | [Golfed](src/aoc2022/Day22Golfed.java) | 2,720 | 841 |
+| 23 | [Source](src/aoc2022/Day23.java) | [Golfed](src/aoc2022/Day23Golfed.java) | 3,302 | 684 |
 | 24 | [Source](src/aoc2022/Day24.java) | [Golfed](src/aoc2022/Day24Golfed.java) | 3,963 | 577 |
 | 25 | [Source](src/aoc2022/Day25.java) | [Golfed](src/aoc2022/Day25Golfed.java) | 669 | 202 |
-| Total |  |  | 45,743 | 12,417 |
+| Total |  |  | 45,743 | 12,385 |
 
 <!-- CHAR COUNTS END -->
--- src/aoc2022/Day22Golfed.java
+++ src/aoc2022/Day22Golfed.java
@@ -3,18 +3,18 @@ class V{
 String[]g;int R;
 String s(boolean p,String[]I){
 g=I;for(;!I[R].isEmpty();R++);
-int x=0,y=g[0].indexOf(46),d=0,X[]={0,1,0,-1},Y[]={1,0,-1,0};
-var q=I[R+1].trim();int l=q.length();
+int x=0,y=g[0].indexOf(46),d=0;
+var q=I[R+1];int l=q.length();
 for(int i=0,n=0,C=0;i<=l;i++)if(i<l&&(C=q.charAt(i))>47&C<58)n=n*10+C-48;else{
 for(;n-->0;){
-int c=d,u=x+X[d],w=y+Y[d];
-if(p)for(;G(u,w)<1;u+=X[d],w+=Y[d]);
+int c=d,X=d%2*(2-d),Y=(1-d%2)*(1-d),u=x+X,w=y+Y;
+if(p)for(;G(u,w)<1;u+=X,w+=Y);
 else if(G(u,w)<1){var r=H(x,y,d);c=r[0];u=r[1];w=r[2];}
-if(G(u,w)==3)break;
+if(G(u,w)<4)break;
 x=u;y=w;d=c;
 }
 x=$.f(x,200);y=$.f(y,200);
-if(i<l)d=d+(C==76?3:1)&3;
+if(i<l)d=d+(C<80?3:1)&3;
 n=0;
 }
 return 1000*++x+4*++y+d+"";
@@ -24,10 +24,10 @@ x=$.f(x,200);y=$.f(y,200);
 return x<R&&y<g[x].length()?g[x].charAt(y)&31:0;
 }
 int[]H(int x,int y,int d){
-int k=(d&1)>0?y%50:x%50,z="!!!!!!P*d9!H!!!!A!1!!!!!!!J%_>!!!!!!F,,!".charAt(4*(x/50*3+y/50)+d)-33,c=z/10%4,b=z%10;
+int k=d%2>0?y%50:x%50,z="!!!!!!P*d9!H!!!!A!1!!!!!!!J%_>!!!!!!F,,!".charAt(4*(x/50*3+y/50)+d)-33,c=z/10%4,b=z%10;
 if(z>39)k=49-k;
 if(z==11&d<2)k=49;
-return A(c,50*(b/3)+((c&1)>0?(c<2?0:49):k),50*(b%3)+((c&1)<1?(c<1?0:49):k));
+return A(c,50*(b/3)+(c%2>0?(c<2?0:49):k),50*(b%3)+(c%2<1?(c<1?0:49):k));
 }
 int[]A(int...a){return a;}
 }
