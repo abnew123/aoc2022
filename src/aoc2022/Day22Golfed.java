77186de5a49f88d445f2a9eab82a7fb0ef786725
abnew123 <shadowghosts31415@gmail.com>
2026-07-18 14:38:11 -0400

    Golf 2022 by 73 characters

--- src/aoc2022/Day22Golfed.java
+++ src/aoc2022/Day22Golfed.java
@@ -9,7 +9,11 @@ for(int i=0,n=0,C=0;i<=l;i++)if(i<l&&(C=q.charAt(i))>47&C<58)n=n*10+C-48;else{
 for(;n-->0;){
 int c=d,X=d%2*(2-d),Y=(1-d%2)*(1-d),u=x+X,w=y+Y;
 if(p)for(;G(u,w)<1;u+=X,w+=Y);
-else if(G(u,w)<1){var r=H(x,y,d);c=r[0];u=r[1];w=r[2];}
+else if(G(u,w)<1){
+int k=d%2>0?y%50:x%50,z="!!!!!!P*d9!H!!!!A!1!!!!!!!J%_>!!!!!!F,,!".charAt(4*(x/50*3+y/50)+d)-33,b=z%10;c=z/10%4;
+k=z==11&d<2?49:z>39?49-k:k;
+u=50*(b/3)+(c%2>0?(c<2?0:49):k);w=50*(b%3)+(c%2<1?(c<1?0:49):k);
+}
 if(G(u,w)<4)break;
 x=u;y=w;d=c;
 }
@@ -22,10 +26,5 @@ return 1000*++x+4*++y+d+"";
 int G(int x,int y){
 x=$.f(x,200);y=$.f(y,200);
 return x<R&&y<g[x].length()?g[x].charAt(y)&31:0;
-}
-int[]H(int x,int y,int d){
-int k=d%2>0?y%50:x%50,z="!!!!!!P*d9!H!!!!A!1!!!!!!!J%_>!!!!!!F,,!".charAt(4*(x/50*3+y/50)+d)-33,c=z/10%4,b=z%10;
-k=z==11&d<2?49:z>39?49-k:k;
-return new int[]{c,50*(b/3)+(c%2>0?(c<2?0:49):k),50*(b%3)+(c%2<1?(c<1?0:49):k)};
 }
 }
