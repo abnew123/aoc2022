02625d166ca273dd5c5da887e56c8ade00a853c4
abnew123 <shadowghosts31415@gmail.com>
2026-07-21 18:08:16 -0400

    Golf days 22 and 23 for 99 characters

--- README.md
+++ README.md
@@ -97,10 +97,10 @@ The table below counts non-whitespace characters in each normal solution file an
 | 19 | [Source](src/aoc2022/Day19.java) | [Golfed](src/aoc2022/Day19Golfed.java) | 2,934 | 736 |
 | 20 | [Source](src/aoc2022/Day20.java) | [Golfed](src/aoc2022/Day20Golfed.java) | 1,029 | 410 |
 | 21 | [Source](src/aoc2022/Day21.java) | [Golfed](src/aoc2022/Day21Golfed.java) | 3,048 | 584 |
-| 22 | [Source](src/aoc2022/Day22.java) | [Golfed](src/aoc2022/Day22Golfed.java) | 2,720 | 742 |
-| 23 | [Source](src/aoc2022/Day23.java) | [Golfed](src/aoc2022/Day23Golfed.java) | 3,302 | 669 |
+| 22 | [Source](src/aoc2022/Day22.java) | [Golfed](src/aoc2022/Day22Golfed.java) | 2,720 | 659 |
+| 23 | [Source](src/aoc2022/Day23.java) | [Golfed](src/aoc2022/Day23Golfed.java) | 3,302 | 653 |
 | 24 | [Source](src/aoc2022/Day24.java) | [Golfed](src/aoc2022/Day24Golfed.java) | 3,963 | 611 |
 | 25 | [Source](src/aoc2022/Day25.java) | [Golfed](src/aoc2022/Day25Golfed.java) | 669 | 210 |
-| Total |  |  | 45,743 | 12,102 |
+| Total |  |  | 45,743 | 12,003 |
 
 <!-- CHAR COUNTS END -->
--- src/aoc2022/Day22Golfed.java
+++ src/aoc2022/Day22Golfed.java
@@ -1,29 +1,26 @@
 package aoc2022;
 class V{
-String[]g;int R,x,y,d;
+String[]g;int R,x,y,d,n;
 String s(boolean p,String[]I){
 for(g=I;!I[R].isEmpty();R++);
 y=g[0].indexOf(46);
-var q=I[R+1]+"LLLL";
-int n=0;for(int C:q.toCharArray())if(C<58)n=n*10+C-48;else{
-for(;n-->0;){
-int c=d,X=d%2*(2-d),Y=(~d&1)*(1-d),u=x+X,w=y+Y;
+for(int C:(I[R+1]+"LLLL").toCharArray())if(C<58)n=n*10+C-48;else{
+for(;n>0;n--){
+int c=d,X=(2-d)%2,Y=(1-d)%2,u=x+X,w=y+Y;
 if(p)for(;G(u,w)<1;u+=X,w+=Y);
 else if(G(u,w)<1){
-int k=d%2>0?y%50:x%50,z="!!!!!!P*d9!H!!!!A!1!!!!!!!J%_>!!!!!!F,,!".charAt(4*(x/50*3+y/50)+d)-33,b=z%10;c=z/10%4;
+int k=(d%2>0?y:x)%50,z="!!!!!!P*d9!H!!!!A!1!!!!!!!J%_>!!!!!!F,,!".charAt(x/50*12+y/50*4+d)-33,b=z%10;c=z/10%4;
 k=z==11&d<2?49:z>39?49-k:k;
-u=50*(b/3)+(c%2>0?(c<2?0:49):k);w=50*(b%3)+(c%2<1?(c<1?0:49):k);
+u=b/3*50+(c%2>0?c/2*49:k);w=b%3*50+(c%2<1?c/2*49:k);
 }
-if(G(u,w)<4)break;
-x=u;y=w;d=c;
+if(G(u,w)>3){x=u;y=w;d=c;}
 }
-x=(Math.floorMod(x,200));y=(Math.floorMod(y,200));
 d=d-C/3&3;
-n=0;
 }
-return 1000*++x+4*++y+d+"";
+return 1000*M(x)+4*M(y)+d+1004+"";
 }
+int M(int v){return(v%200+200)%200;}
 int G(int x,int y){
-return(x=(Math.floorMod(x,200)))<R&&(y=(Math.floorMod(y,200)))<g[x].length()?g[x].charAt(y)&31:0;
+return(x=M(x))<R&&(y=M(y))<g[x].length()?g[x].charAt(y)&31:0;
 }
 }
