6c8218bef25d52d9e97302966332d33a156b912f
abnew123 <shadowghosts31415@gmail.com>
2026-07-23 13:25:59 -0400

    Golf days 16, 17 and 24 for 42 characters

--- src/aoc2022/Day17Golfed.java
+++ src/aoc2022/Day17Golfed.java
@@ -1,18 +1,18 @@
 package aoc2022;
 class Q{
-int P[]={15,33666,66055,2113665,387},h,k,q;java.math.BigInteger c,B;
+int P[]={15,33666,66055,2113665,387},h,k,q,y;java.math.BigInteger c,B;
 String s(boolean p,String[]j){
-var m=new java.util.HashMap();long n=p?2022:(long)1e12,r=0,e=0;
+var m=new java.util.HashMap();long n=p?2022:(long)1e12,r=0,e=0,a;
 for(c=c.ZERO,k=q=~(h=0);r++<n;){
-B=c.valueOf(P[k=++k%5]);int x=2,y=h+3,X;
-do x=o(X=x+j[0].charAt(q=++q%j[0].length())-61,y)?X:x;while(o(x,--y));
+B=c.valueOf(P[k=++k%5]);int x=2,X;y=h+3;
+do x=o(X=x+j[0].charAt(q=++q%j[0].length())-61)?X:x;while(o(x+--y-y));
 X=h=((c=c.or(B.shiftLeft(x+7*++y))).bitLength()+6)/7;
-for(x=7;x-->0;X=y<0|y>=X?X:y+1)for(y=h;y-->0&&!c.testBit(7*y+x););
-if(e<1&&m.put(q*5+k+":"+c.shiftRight(7*X),new long[]{r,h})instanceof long[]v){long a=r-v[0],g=(n-r)/a;r+=g*a;e=g*(h-v[1]);}
+for(x=7;x-->0;X=++y>0&y<X?y:X)for(y=h;y-->0&&!c.testBit(7*y+x););
+if(e<1&&m.put(q+":"+k+c.shiftRight(7*X),new long[]{r,h})instanceof long[]v){e=(n-r)/(a=r-v[0])*(h-v[1]);r=n-(n-r)%a;}
 }
 return e+h+"";
 }
-boolean o(int x,int y){
+boolean o(int x){
 return(x|y|(23843>>3*k&7)-x)>=0&c.and(B.shiftLeft(x+7*y)).signum()<1;
 }
 }
--- src/aoc2022/Day24Golfed.java
+++ src/aoc2022/Day24Golfed.java
@@ -1,6 +1,6 @@
 package aoc2022;
 class X{String[]g;int R,C,x;
-int h(int u,int v){int m=R-2,k=C-2;return g[1+(x/C+u%m+m-1)%m].charAt(1+(x%C+v%k+k-1)%k);}
+int h(int u,int v){int m=R-2,k=C-2;return g[1+(x/C+u%m+m-2)%m].charAt(1+(x%C+v%k+k-1)%k);}
 String s(boolean p,String[]I){
 g=I;R=g.length;C=g[0].length();
 int a=g[0].indexOf(46)+C,b=R*C+g[R-1].indexOf(46);
@@ -8,11 +8,8 @@ return""+f(a,b,p?0:f(b,a,f(a,b,0)));
 }
 int f(int a,int b,int t){
 var q=new int[2*R*C];
-for(q[a]=1;q[b]<1;){
-var n=new int[2*R*C];t++;
-for(x=R*C;x-->0;)n[x+C]=g[x/C].charAt(x%C)>35&(x/C%~-R<1|h(t,0)!=94&h(-t,0)<118&h(0,t)!=60&h(0,-t)!=62)?q[x]|q[x+C]|q[x+C+C]|q[x+C-1]|q[x+C+1]:0;
-q=n;
-}
+for(q[a]=1;q[b]<1;)
+for(t++,x=R*C+C;x-->C;)q[x]=q[x]*2|(g[x/C-1].charAt(x%C)>35&(x/C%~-R==1|h(t,0)!=94&h(-t,0)<95&h(0,t)!=60&h(0,-t)!=62)?(q[x+1]|q[x+C])>>1|q[x]|q[x-1]|q[x-C]:0)&1;
 return t;
 }
 }
