ed459d2fa508d64f9d59c33ccc74c580e2ca2d22
abnew123 <shadowghosts31415@gmail.com>
2026-07-23 09:59:39 -0400

    Golf days 13, 14, 15, 16, 17, 22 and 23 for 169 characters

--- src/aoc2022/Day17Golfed.java
+++ src/aoc2022/Day17Golfed.java
@@ -3,16 +3,12 @@ class Q{
 int P[]={15,33666,66055,2113665,387},h,k,q;java.math.BigInteger c,B;
 String s(boolean p,String[]j){
 var m=new java.util.HashMap();long n=p?2022:(long)1e12,r=0,e=0;
-for(c=c.ZERO,q=~(h=0);r<n;){
-B=c.valueOf(P[k=(int)(r++%5)]);int x=2,y=h+3,X;
+for(c=c.ZERO,k=q=~(h=0);r++<n;){
+B=c.valueOf(P[k=++k%5]);int x=2,y=h+3,X;
 do x=o(X=x+j[0].charAt(q=++q%j[0].length())-61,y)?X:x;while(o(x,--y));
-c=c.or(B.shiftLeft(x+7*++y));h=(c.bitLength()+6)/7;
-if(e<1){
-X=h;
+X=h=((c=c.or(B.shiftLeft(x+7*++y))).bitLength()+6)/7;
 for(x=7;x-->0;X=y<0|y>=X?X:y+1)for(y=h;y-->0&&!c.testBit(7*y+x););
-var v=(long[])m.put(q*5+k+":"+c.shiftRight(7*X),new long[]{r,h});
-if(v!=null){long a=r-v[0],g=(n-r)/a;r+=g*a;e=g*(h-v[1]);}
-}
+if(e<1&&m.put(q*5+k+":"+c.shiftRight(7*X),new long[]{r,h})instanceof long[]v){long a=r-v[0],g=(n-r)/a;r+=g*a;e=g*(h-v[1]);}
 }
 return e+h+"";
 }
--- src/aoc2022/Day22Golfed.java
+++ src/aoc2022/Day22Golfed.java
@@ -7,7 +7,7 @@ y=g[0].indexOf(46);
 for(int C:(I[R]+"<").getBytes())if(C<58)n=n*10+C-48;else{
 for(;n>0;n--){
 int c=d,u=x,w=y;
-for(;G(u+=(2-d)%2,w+=(1-d)%2)<33;)if(!p){
+for(;G(u=u+(2-d)%2&255,w=w+(1-d)%2&255)<33;)if(!p){
 int k=(d%2>0?y:x)%50,z="PPPPPPW1k@P'PPPPHP8PPPPPPPy,fEPPPPPPM33P".charAt(x/50*12+y/50*4+d);c=z/10%4;
 k=z<52&d<2?49:z>80?49-k:k;
 u=z%10/3*50+(c%2>0?c/2*49:k);w=z%10%3*50+(c%2<1?c/2*49:k);
@@ -17,10 +17,9 @@ if(G(u,w)>35){x=u;y=w;d=c;}
 }
 d=d-C/3&3;
 }
-return 1000*M(x)+4*M(y)+d+1004+"";
+return 1000*x+4*y+d+1004+"";
 }
-int M(int v){return(v%200+200)%200;}
 int G(int x,int y){
-return(x=M(x))<R&&(y=M(y))<g[x].length()?g[x].charAt(y):0;
+return x<R&&y<g[x].length()?g[x].charAt(y):0;
 }
 }
