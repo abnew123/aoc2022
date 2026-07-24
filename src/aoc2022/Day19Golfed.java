59e2486b555ebda5070d91eca605ebd1bd9931e7
abnew123 <shadowghosts31415@gmail.com>
2026-07-23 23:39:20 -0400

    Golf days 19, 22, 23 and 24 for 16 characters

--- src/aoc2022/Day19Golfed.java
+++ src/aoc2022/Day19Golfed.java
@@ -8,7 +8,7 @@ return""+r;
 void d(int t,int...s){
 for(int i=(z=z<s[7]?s[7]:z)<s[7]+t*t/2?4:0;i-->0;){
 int w=0,j,y;
-for(j=3;j-->0;)if((y=q[i*3+j]-s[4+j])>0&w<(y=s[j]<1?t:~-y/s[j]+1))w=y;
+for(j=3;j-->0;w=(y=q[i*3+j]-s[4+j])>0&w<(y=s[j]<1?t:~-y/s[j]+1)?y:w);
 if(w++<t){var u=s.clone();u[i]++;u[7]+=i/3*(t-w);for(j=3;j-->0;)u[4+j]+=s[j]*w-q[i*3+j];d(t-w,u);}
 }
 }
--- src/aoc2022/Day24Golfed.java
+++ src/aoc2022/Day24Golfed.java
@@ -9,7 +9,7 @@ return""+f(a,b,p?0:f(b,a,f(a,b,0)));
 int f(int a,int b,int t){
 var q=new int[2*R*C];
 for(q[a]=1;q[b]<1;)
-for(t++,x=R*C+C;x-->C;)q[x]=q[x]*2|(g[x/C-1].charAt(x%C)>35&(x/C%~-R==1|h(t,0)!=94&h(-t,0)<95&h(0,t)!=60&h(0,-t)!=62)?(q[x+1]|q[x+C])>>1|q[x]|q[x-1]|q[x-C]:0)&1;
+for(t++,x=R*C+C;x-->C;)q[x]=q[x]*2|(g[x/C-1].charAt(x%C)>35&(x==a|x==b|h(t,0)!=94&h(-t,0)<95&h(0,t)!=60&h(0,-t)!=62)?(q[x+1]|q[x+C])>>1|q[x]|q[x-1]|q[x-C]:0)&1;
 return t;
 }
 }
